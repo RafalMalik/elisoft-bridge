350812b09a6af6841630c197729aea5abe9b85d6
RafalMalik <rafalmalik.info@gmail.com>
2019-02-20 08:40:44 +0100

    Init repo

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php  
+//$serverName = "ServerName"; 
+//$uid = "sqlusername";   
+//$pwd = "sqlpassword";  
+//$databaseName = "DBName"; 
+
+//$connectionInfo = array( "UID"=>$uid,                            
+    //                     "PWD"=>$pwd,                            
+  //                       "Database"=>$databaseName); 
+
+/* Connect using SQL Server Authentication. */  
+//$conn = sqlsrv_connect( $serverName, $connectionInfo);  
+
+//$tsql = "SELECT id, FirstName, LastName, Email FROM tblContact";  
